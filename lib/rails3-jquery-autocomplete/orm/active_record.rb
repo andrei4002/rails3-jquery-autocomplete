e659448879b0c8fce0aa09e6032bcebf63744521
Andrei <andrei@ubuntu.(none)>
2012-06-09 18:15:49 +0300

    added joins option

--- lib/rails3-jquery-autocomplete/orm/active_record.rb
+++ lib/rails3-jquery-autocomplete/orm/active_record.rb
@@ -15,6 +15,7 @@ module Rails3JQueryAutocomplete
         options = parameters[:options]
         scopes  = Array(options[:scopes])
         where   = options[:where]
+        joins   = options[:joins]
         limit   = get_autocomplete_limit(options)
         order   = get_autocomplete_order(method, options, model)
 
@@ -24,6 +25,7 @@ module Rails3JQueryAutocomplete
         scopes.each { |scope| items = items.send(scope) } unless scopes.empty?
 
         items = items.select(get_autocomplete_select_clause(model, method, options)) unless options[:full_model]
+        items = items.joins(joins) if joins.present?
         items = items.where(get_autocomplete_where_clause(model, term, method, options)).
             limit(limit).order(order)
         items = items.where(where) unless where.blank?
